f5973519aff37ecfe22dedde7b139edc2e2c3f4d
drWU <jthomascopp@gmail.com>
2021-03-10 20:05:10 -0500

    new version

--- collector.py
+++ collector.py
@@ -1,19 +1,19 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import config
-import time
+import time, datetime
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('songHistory')
+table = dynamodb.Table('songHistory2')
 
 
 sp = spotipy.Spotify(auth_manager=config.sp)
 
-interval = 0.2
-#x = 1000
+interval = 0.4
 starttime = time.time()
 
+#get initial song playing
 temp = sp.current_playback()
 
 #create dictionary
@@ -22,47 +22,55 @@ lastSong = {
     "artistName" : '',
     "albumName" : '',
     "msPlayed" : 999999999,
-    "endTime" : ''
+    "timePlayed" : 9999999
 }
 #fill dictionary with currently playing 
 if (temp != None) :
-    t = time.localtime()
+    uTime = int(time.time())
     lastSong['trackName'] = temp['item']['name']
     lastSong['artistName'] = temp['item']['artists'][0]['name']
     lastSong['albumName'] = temp['item']['album']['name']
     lastSong['msPlayed'] = temp['progress_ms']
-    lastSong['endTime'] = str(t[0]) + "-" + str(t[1]) + "-" + str(t[2]) + " " + str(t[3]) + ":" + str(t[4]) + ":" + str(t[5])
+    lastSong['timePlayed'] = uTime
+
 
 #run indefinetely until ctrl c
 while True:
 
     time.sleep(interval - ((time.time() - starttime) % interval))
-    #x -= interval
+
     temp = sp.current_playback()
+    
+    uTime = int(time.time())
+    
     #make sure user is actually playing something
-    if (temp != None) :
-
-        t = time.localtime()
-
+    #passes if temp is empty
+    try :
         #once songs changes
         if(temp['progress_ms'] < lastSong['msPlayed']) :
+            
             #pass if it detects pause
             if (-temp['progress_ms'] * 0.001 + lastSong['msPlayed'] * 0.001) < 1.5 :
                 pass
             #a new song
             else : 
                 #add new item in database
-                print(lastSong)
+                print("added --->", lastSong)
+                print("at --->", time.ctime(lastSong['timePlayed']))
                 table.put_item(Item={
-                'endTime' : lastSong['endTime'],
+                'timePlayed' : lastSong['timePlayed'],
                 'trackName' : lastSong['trackName'],
                 'artistName' : lastSong['artistName'],
                 'albumName' : lastSong['albumName'],
                 'msPlayed' : lastSong['msPlayed']
                 })
-
         lastSong['trackName'] = temp['item']['name']
         lastSong['artistName'] = temp['item']['artists'][0]['name']
         lastSong['albumName'] = temp['item']['album']['name']
         lastSong['msPlayed'] = temp['progress_ms']
-        lastSong['endTime'] = str(t[0]) + "-" + str(t[1]) + "-" + str(t[2]) + " " + str(t[3]) + ":" + str(t[4]) + ":" + str(t[5])
+        lastSong['timePlayed'] = uTime
+    except(TypeError):
+        pass
+#uTime = time.time()
+#print(uTime)
+#print(time.ctime(uTime))
